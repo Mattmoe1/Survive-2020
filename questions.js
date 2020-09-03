edcd131d7607349dcb79d1a35dca34805b1acd4b
kjmckinley <67598876+kjmckinley@users.noreply.github.com>
2020-09-02 19:35:13 -0500

    added comments and function calls

--- questions.js
+++ questions.js
@@ -1,3 +1,5 @@
+
+//array of objects that will refer to the options.
 var questions = [
     {
         title: "You wake up. You're groggy, as though you had a vivid dream but you can't quite remember what it was about.You grab your phone and begin flipping through the news. The first article blares in all caps 'BREAKING NEWS!' followed by: 'This just in! The COVID-19 virus has just been officially declared a pandemic by the United States. People are advised to stay home. Do not panic. Wash your hands thoroughly and maintain a minimum of 6ft distance from others. People are advised to avoid crowded public areas due to risk of infection. Again, do not panic.'",
@@ -13,14 +15,17 @@ var questions = [
 
 var currentIndex = 0;
 
+//function that displays the narrative/question
 function displayQuestion(){
 var h2 = $("<h2>").text("question:" + questions[currentIndex].title)
 console.log("question:" + questions[currentIndex].title)
 $(".question-container").append(h2);
 
+btnOptions();
 
 }
 
+//function that generates the options in the object array
 function btnOptions() {
     $("#choice-screen")
     questions[currentIndex].choices.forEach(function(choice, i){
@@ -31,8 +36,9 @@ function btnOptions() {
     $(".question-container").append(btn);
 }
 
+//function that runs the next feature
 function nextBtn () {
-    
+
 }
 
 
@@ -52,7 +58,6 @@ function nextBtn () {
     
 
 //}
-// makeChoice();
 
 // $("#first-choice").on("click", function(){
 //     $(".bad-ending").removeClass("hide")
